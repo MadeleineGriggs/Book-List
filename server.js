e16b2b0610f3b53978f83c1846c7b615714719ef
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-10-17 20:44:55 -0400

    added connection for mongoose

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://username:password1@ds235658.mlab.com:35658/heroku_xcc23xkg");
 
 // Start the API server
 app.listen(PORT, function() {
